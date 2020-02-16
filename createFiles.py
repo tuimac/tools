ad5162b0aed5b963510deba60ab76cf85c4ce687
tuimac <tuimac.devadm01@gmail.com>
2020-02-16 17:41:35 +0900

    Just Created

---
+++ createFiles.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from multiprocessing import Process, cpu_count
+from random import gauss
+from os import statvfs, urandom, mkdir
+import cPickle
+
+def writeObject(filepath):
+    # Unit is byte.
+    data_size = 104857600
+    f = open(filepath, 'wb')
+    cPickle.dump(urandom(data_size))
+    f.close()
+
+def manageWrite(basename):
+    dir = "/data/"
+    fileindex = 1
+    stat = statvfs(dir)
+    dir = dir + basename + "dir/"
+    mkdir(dir)
+
+    while True:
+        free_space = stat.f_frsize * stat.f_blocks
+        if free_space < 1000: break
+        filepath = dir + basename + "-" + str(fileindex)
+        writeObject(filepath)
+        fileindex += 1
+    print "Process has been finished."
+
+def main():
+    filename = "test-"
+    cores = cpu_count()
+    process_list = []
+    for core in range(cores):
+        basename = filename + str(core) + "-"
+        p = Process(target=manageWrite, args=(basename,))
+        p.start()
+        process_list.append(p)
+    [p.join() for p in process_list]
+
+if __name__ == '__main__':
+    main()
