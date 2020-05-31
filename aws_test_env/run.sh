c4b92eadc271b676e6e784a711933f73cd5e09c2
tuimac <tuimac.devadm01@gmail.com>
2020-05-31 13:21:32 +0900

    add run script

--- aws_test_env/run.sh
+++ aws_test_env/run.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
-[[ -z $1 ]] && { echo "Need argument as a name."; exit 1; }
 
-NAME=$1
+NAME="test-aws"
 
-docker build -t ${NAME} .
-docker run -itd --name ${NAME} \
-            -v "/var/run/docker.sock:/var/run/docker.sock" \
-            -v "/usr/bin/docker:/usr/bin/docker" \
-            ${NAME}
+function delete_docker(){
+    docker stop ${NAME}
+    docker rm ${NAME}
+    docker rmi ${NAME}
+}
 
+function create_docker(){
+    docker build -t ${NAME} .
+    docker run -itd --name ${NAME} \
+                -v "/var/run/docker.sock:/var/run/docker.sock" \
+                -v "/usr/bin/docker:/usr/bin/docker" \
+                -v "volume:/root" \
+                ${NAME}
+}
 
+function main(){
+    [[ -z $1 ]] && { echo "Need argument which is 'create' or 'delete'."; exit 1; }
+    if [ $1 == "create" ]; then
+        create_docker
+    else
+        delete_docker
+    fi
+}
+
+main $1
 
 
 
