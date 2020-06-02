948d23d4f05da59bebb6f39644725afa62c52147
tuimac <tuimac.devadm01@gmail.com>
2020-06-02 10:58:02 +0900

    fixed

--- aws_test_env/run.sh
+++ aws_test_env/run.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-
 NAME="test-aws"
+VOLUME="${PWD}/volume"
 
 function delete_docker(){
     docker stop ${NAME}
     docker rm ${NAME}
     docker rmi ${NAME}
+    docker image prune -f
+    rm -rf ${VOLUME}
 }
 
 function create_docker(){
+    mkdir ${VOLUME}
     docker build -t ${NAME} .
     docker run -itd --name ${NAME} \
                 -v "/var/run/docker.sock:/var/run/docker.sock" \
                 -v "/usr/bin/docker:/usr/bin/docker" \
-                -v "volume:/root" \
-                ${NAME}
+                -v "${VOLUME}:/tmp" \
+                ${NAME} /bin/bash
 }
 
 function main(){
     [[ -z $1 ]] && { echo "Need argument which is 'create' or 'delete'."; exit 1; }
     if [ $1 == "create" ]; then
         create_docker
-    else
+    elif [ $1 == "delete" ]; then
         delete_docker
+    else
+        { echo "Need argument which is 'create' or 'delete'."; exit 1; }
     fi
 }
 
