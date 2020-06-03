3ea8629c3aeeb90e68edf09a3c8ed720147d5741
tuimac <tuimac.devadm01@gmail.com>
2020-06-03 15:54:29 +0900

    fixed

--- aws/docker/run.sh
+++ aws/docker/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NAME="aws-test"
+NAME="awstest"
 VOLUME="${PWD}/volume"
 DOCKERHUBUSER="tuimac"
 
@@ -14,7 +14,7 @@ function deleteAll(){
 
 function createContainer(){
     mkdir ${VOLUME}
-    docker build -t ${NAME} .
+    docker build -t ${DOCKERHUBUSER}/${NAME} .
     docker run -itd --name ${NAME} \
                 -v "/var/run/docker.sock:/var/run/docker.sock" \
                 -v "/usr/bin/docker:/usr/bin/docker" \
@@ -24,29 +24,58 @@ function createContainer(){
 
 function commitImage(){
     docker stop ${NAME}
-    docker commit ${NAME} ${NAME}
-    cat password.txt | docker login --username ${DOCKERHUBUSER} --password-stdin
+    docker commit ${NAME} ${DOCKERHUBUSER}/${NAME}
+    cat password.txt | base64 -d | docker login --username ${DOCKERHUBUSER} --password-stdin
     if [ $? -ne 0 ]; then
-        echo -ne "Password: "
-        read -s password
-        echo
-        docker login --username ${DOCKERHUBUSER} --password $password
+        docker login --username ${DOCKERHUBUSER}
     fi
     docker push ${DOCKERHUBUSER}/${NAME}
     docker start ${NAME}
 }
 
+function registerSecret(){
+        if [ -e password.txt ]; then
+        echo -en "There is 'password.txt' file in your current directory."
+        echo -en "Continue this? [y/n]: "
+        read answer
+        if [ $answer == "n" ]; then
+            echo "Registering password is skipped."
+            exit 0
+        else
+            echo "Only type in 'y' or 'n'."
+            exit 1
+        fi
+        fi
+        echo -en "Password: "
+        read -s password
+        echo
+        echo $password | base64 > password.txt
+        cat password.txt | base64 -d
+}
+
+function userguide(){
+    echo -e "usage: ./run.sh [help | create | delete | commit | register-secret]"
+    echo -e "
+        optional arguments:
+        create              Create image and container after that run the container.
+        delete              Delete image and container.
+        commit              Create image from target container and push the image to remote repository.
+        register-secret     Create password.txt for make it login process within 'commit' operation.
+    "
+}
+
 function main(){
-    local message="Need argument which is 'create', 'commit' or 'delete'."
-    [[ -z $1 ]] && { echo $message; exit 1; }
+    [[ -z $1 ]] && { userguide; exit 1; }
     if [ $1 == "create" ]; then
         createContainer
     elif [ $1 == "delete" ]; then
         deleteAll
     elif [ $1 == "commit" ]; then
         commitImage
+    elif [ $1 == "help" ]; then
+        userguide
     else
-        { echo $message; exit 1; }
+        { userguide; exit 1; }
     fi
 }
 
