0fe7b469444886aff3f047470add148fcc8a5c9a
tuimac <tuimac.devadm01@gmail.com>
2020-08-14 00:45:31 +0900

    add new

---
+++ powershell/rsync.ps1
@@ -0,0 +1,39 @@
+function loginRemoteServer($remoteInfo){
+    while($true){
+        Get-ChildItem -Path $remoteInfo['dest'] 2>&1 | Out-Null
+        if($?){
+            break
+        }else{
+            sleep 1
+        }
+    }
+}
+
+function rsync($remoteInfo){
+    $arg = '/COPY:DAT /MIR /R:0 /W:' + [string]$remoteInfo['interval']
+    while($true){
+        robocopy $remoteInfo['src'] $remoteInfo['dest']
+        if($?){
+            sleep $remoteInfo['interval']
+        }else{
+            break
+        }
+
+    }
+}
+
+function main(){
+    $remoteInfo = @{
+        username = 'administrator'
+        password = 'P@ssw0rd'
+        src = 'C:\tmp'
+        dest = '\\IPorHostname\tmp'
+        interval = 1
+    }
+
+    loginRemoteServer $remoteInfo
+    rsync $remoteInfo
+
+}
+
+main
