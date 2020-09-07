914be6671c6ba56f353ef11294058fe7a00c2a00
tuimac <tuimac.devadm01@gmail.com>
2020-09-07 19:41:41 +0900

    update config

--- aws/eks/ekscluster.sh
+++ aws/eks/ekscluster.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 
-CLUSTER=ac-test
-NODEGROUP=ac-test-a
+CLUSTER=test
+NODEGROUP=test-a
 
 function create(){
     aws eks create-cluster \
-        --name $CLUSTER \
-        --role-arn arn:aws:iam::500415866962:role/EKS-cluster \
+        --name ${CLUSTER} \
+        --role-arn arn:aws:iam::000000000000:role/EKS-cluster-role \
         --resources-vpc-config \
-            subnetIds=subnet-0fc7211f4023b71ff,subnet-0972f112479448909,securityGroupIds=sg-0810dfe81aefef4f2,endpointPrivateAccess=true,endpointPublicAccess=false \
+            subnetIds=subnet-xxxxxxxxxxxxxxxxx,securityGroupIds=sg-xxxxxxxxxxxxxxxxxx,endpointPrivateAccess=true,endpointPublicAccess=false \
         --kubernetes-version 1.17
 }
 
-function nodegroup(){
+function nodegroup_eksctl(){
+        eksctl create nodegroup \
+          --cluster ${CLUSTER} \
+          --region ap-northeast-1 \
+          --name ${NODEGROUP} \
+          --node-type t3.small \
+          --nodes 2 \
+          --nodes-min 2 \
+          --nodes-max 2 \
+          --ssh-access \
+          --ssh-public-key test.pem \
+          --managed
+}
+
+function nodegroup_awscli(){
     aws eks create-nodegroup \
-        --cluster-name $CLUSTER \
-        --nodegroup-name $NODEGROUP \
+        --cluster-name ${CLUSTER} \
+        --nodegroup-name ${NODEGROUP} \
         --scaling-config minSize=2,maxSize=2,desiredSize=2 \
-        --disk-size 30 \
-        --subnets subnet-0fc7211f4023b71ff subnet-0972f112479448909 \
+        --disk-size 20 \
+        --subnet subnet-xxxxxxxxxxxxxxxxxx subnet-xxxxxxxxxxxxxxxxxx \
         --instance-type t3.small \
         --ami-type AL2_x86_64 \
-        --remote-access ec2SshKey=tuimac,sourceSecurityGroups=sg-0810dfe81aefef4f2 \
-        --node-role arn:aws:iam::500415866962:role/EC2-Admin \
+        --remote-access ec2SshKey=test,sourceSecurityGroups=sg-xxxxxxxxxxxxxxxxxx \
+        --node-role arn:aws:iam::000000000000:role/EC2-EKS-role \
         --labels Name=${NODEGROUP} \
         --tags Name=${NODEGROUP}
 }
@@ -30,14 +44,16 @@ function nodegroup(){
 function update(){
     aws eks update-kubeconfig \
         --region ap-northeast-1 \
-        --name $CLUSTER
+        --name ${CLUSTER}
+        #--role-arn arn:aws:iam::000000000000:role/EC2-EKS-role
 }
 
 function main(){
+    [[ -z $1 ]] && { echo 'Need argument.'; exit 1; }
     if [ $1 == 'create' ]; then
         create
     elif [ $1 == 'nodegroup' ]; then
-        nodegroup
+        nodegroup_awscli
     elif [ $1 == 'update' ]; then
         update
     else
