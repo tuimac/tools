4868bad84dda1f7916d0a7840b55220e1007d05b
tuimac <tuimac.devadm01@gmail.com>
2020-09-19 11:11:44 +0900

    fixed

--- vim/installToLinux.sh
+++ vim/installToLinux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo mkdir /etc/vim/undo
-sudo mkdir /etc/vim/backup
+sudo mkdir -p /etc/vim/undo
+sudo mkdir -p /etc/vim/backup
 curl -L https://raw.githubusercontent.com/tuimac/tools/master/vim/vimrc -o /etc/vimrc
 sudo chmod -R 777 /etc/vim
