a7a77688494c7ba2d08e37b566421efb72e80420
tuimac <tuimac.devadm01@gmail.com>
2020-09-27 21:44:44 +0900

    fixed authorization

--- vim/installToLinux.sh
+++ vim/installToLinux.sh
@@ -2,5 +2,6 @@
 
 sudo mkdir -p /etc/vim/undo
 sudo mkdir -p /etc/vim/backup
-curl -L https://raw.githubusercontent.com/tuimac/tools/master/vim/vimrc -o /etc/vimrc
+sudo rm /etc/vimrc
+sudo curl -L https://raw.githubusercontent.com/tuimac/tools/master/vim/vimrc -o /etc/vimrc
 sudo chmod -R 777 /etc/vim
