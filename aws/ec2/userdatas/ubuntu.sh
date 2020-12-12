d8d34686074d9edca441e18742692bdbd47da4ba
tuimac <tuimac.devadm01@gmail.com>
2020-12-12 05:30:59 +0000

    fixed detail

--- aws/ec2/userdatas/ubuntu.sh
+++ aws/ec2/userdatas/ubuntu.sh
@@ -14,7 +14,7 @@ exec >> $LOG 2>&1
     apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common git
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    apt install docker-ce docker-ce-cli containerd.io
+    apt install docker-ce docker-ce-cli containerd.io -y
     systemctl enable docker
     usermod -aG docker ubuntu
     curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
