1552b9b947894022219f213ab4e853d26973537b
tuimac <tuimac.devadm01@gmail.com>
2020-12-19 07:58:37 +0000

    fixed

--- docker/install-buildx.sh
+++ docker/install-buildx.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+[[ ! -e 'Dockerfile' ]] && { echo 'There is no Dockerfile on your directory.'; exit 1; }
+
 git clone git://github.com/docker/buildx && cd buildx
 make install
 export DOCKER_BUILDKIT=1
 docker build --platform=local -o . git://github.com/docker/buildx
 mkdir -p ~/.docker/cli-plugins
 mv buildx ~/.docker/cli-plugins/docker-buildx
+export DOCKER_CLI_EXPERIMENTAL=enabled
+docker run --rm --privileged docker/binfmt
+cat /proc/sys/fs/binfmt_misc/qemu-aarch64
+docker buildx create --use tuimac
+
+docker login
+
+echo -en 'Enter Image Name: '
+read IMAGENAME
+
+docker buildx build --platform linux/amd64,linux/arm64 -t ${IMAGENAME} --push .
