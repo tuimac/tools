98321333120c5d13595ac7bf99559b109010a768
tuimac <tuimac.devadm01@gmail.com>
2020-12-30 00:22:02 +0000

    add new

--- aws/ec2/userdatas/nodejs-amzn2.sh
+++ aws/ec2/userdatas/nodejs-amzn2.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 LOG=/var/log/user-data.log
-HOSTNAME=nodejs
+HOSTNAME=dns
 DOMAIN=tuimac.private
 touch $LOG
 exec >> $LOG 2>&1
-    yum update -y
-    mkdir -p /etc/vim/undo
-    mkdir -p /etc/vim/backup
-    rm /etc/vimrc
-    curl -L https://raw.githubusercontent.com/tuimac/tools/master/vim/vimrc -o /etc/vimrc
+    apt update -y
+    apt upgrade -y
+    mkdir /etc/vim/undo
+    mkdir /etc/vim/backup
+    rm /etc/vim/vimrc
+    curl -L https://raw.githubusercontent.com/tuimac/tools/master/vim/vimrc -o /etc/vim/vimrc
     chmod -R 777 /etc/vim
-    echo 'if [ -n "$BASH_VERSION" -o -n "$KSH_VERSION" -o -n "$ZSH_VERSION" ]; then
-      [ -x /usr/bin/id ] || return
-      ID=`/usr/bin/id -u`
-      #[ -n "$ID" -a "$ID" -le 200 ] && return
-      # for bash and zsh, only if no alias is already set
-      alias vi >/dev/null 2>&1 || alias vi=vim
-    fi' > /etc/profile.d/vim.sh
+    curl -sL https://deb.nodesource.com/setup_12.x | bash -
+    export NG_CLI_ANALYTICS=ci
+    npm install -g @angular/cli
+    apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common git nodejs
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    apt install docker-ce docker-ce-cli containerd.io -y
+    systemctl enable docker
+    usermod -aG docker ubuntu
+    curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
     IP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
     cat <<EOF >> /etc/hosts
 $IP $HOSTNAME ${HOSTNAME}.${DOMAIN}
-    echo ${HOSTNAME}.${DOMAIN} > /etc/hostname
-    curl -sL https://deb.nodesource.com/setup_12.x | bash -
-    apt install nodejs
-    export NG_CLI_ANALYTICS=ci
-    npm install -g @angular/cli
 EOF
+    echo ${HOSTNAME}.${DOMAIN} > /etc/hostname
+    cd /home/ec2-user
+    su ec2-user -c 'https://github.com/tuimac/serverless_sample.git'
     reboot
