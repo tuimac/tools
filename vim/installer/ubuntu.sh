aed2848c10fcbc3949cf89fcf38a10ce4baef0fd
tuimac <tuimac.devadm01@gmail.com>
2021-01-09 06:34:02 +0000

    fixed for upgrading f

--- vim/installer/ubuntu.sh
+++ vim/installer/ubuntu.sh
@@ -3,5 +3,5 @@
 mkdir -p /etc/vim/undo
 mkdir -p /etc/vim/backup
 rm /etc/vim/vimrc.local
-curl -L https://raw.githubusercontent.com/tuimac/tools/master/vim/vimrc -o /etc/vim/vimrc.local
+curl -L https://raw.githubusercontent.com/tuimac/tools/master/vim/vimrc -o /etc/vim/vimrc
 chmod -R 777 /etc/vim
