39ad54b35aafd5b96dc37fbaea14b147e196ac7a
tuimac <tuimac.devadm01@gmail.com>
2021-02-06 06:48:37 +0000

    fixed dns resolving bugs

--- aws/ec2/userdatas/ubuntu.sh
+++ aws/ec2/userdatas/ubuntu.sh
@@ -5,16 +5,18 @@ DOMAIN=tuimac.private
 touch $LOG
 exec >> $LOG 2>&1
     apt update
+    apt install -y docker.io git curl
     apt upgrade -y
     mkdir -p /etc/vim/undo
     mkdir -p /etc/vim/backup
     chmod -R 777 /etc/vim
-    apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common git
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    apt install docker-ce docker-ce-cli containerd.io -y
     systemctl enable docker
     usermod -aG docker ubuntu
+    for i in {0..10};do
+      curl https://github.com
+      [[ $? -eq 0 ]] && { break; }
+      sleep 6
+    done
     curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
     IP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
