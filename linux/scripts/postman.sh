770d3b5122af2d09ceb8ea3a8ab3ad79f6481b04
tuimac <tuimac.devadm01@gmail.com>
2021-02-27 04:43:48 +0000

    fixed parameter

--- linux/scripts/postman.sh
+++ linux/scripts/postman.sh
@@ -3,5 +3,5 @@
 [[ -z $1 ]] && { echo 'There is no argument.'; exit 1; }
 
 curl -X POST -H "Content-Type: application/json" \
-    -d '{"Name": "test", "test": "test"}' \
+    -d '{"Name": "test", "command": "docker ps -a"}' \
     $1 | jq
