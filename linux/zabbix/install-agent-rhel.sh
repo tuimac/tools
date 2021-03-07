19b9f162653681e8dae4d8237d1da892b0132df7
tuimac <tuimac.devadm01@gmail.com>
2021-03-07 06:42:31 +0000

    change for zabbix agent2

--- linux/zabbix/install-agent-rhel.sh
+++ linux/zabbix/install-agent-rhel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sed -i -e "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
+sudo sed -i -e "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
 # Get url from https://repo.zabbix.com/zabbix
 sudo dnf install https://repo.zabbix.com/zabbix/5.0/rhel/8/x86_64/zabbix-agent2-5.0.8-1.el8.x86_64.rpm
-sudo dnf install zabbix-agent
+sudo dnf install -y zabbix-agent2
 
-CONF='/etc/zabbix/zabbix_agentd.conf'
-sed -i -e "s/Server=127.0.0.1/Server=10.3.0.230/g" ${CONF}
-sed -i -e "s/ServerActive=127.0.0.1/ServerActive=10.3.0.230/g" ${CONF}
-systemctl enable zabbix-agent
-systemctl start zabbix-agent
+CONF='/etc/zabbix/zabbix_agent2.conf'
+sudo sed -i -e "s/Server=127.0.0.1/Server=10.3.0.230/g" ${CONF}
+sudo sed -i -e "s/ServerActive=127.0.0.1/ServerActive=10.3.0.230/g" ${CONF}
+sudo systemctl enable zabbix-agent2
+sudo systemctl start zabbix-agent2
