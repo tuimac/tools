387665da21b5db34103efd89ce7806a16d9d0a02
tuimac <tuimac.devadm01@gmail.com>
2021-05-29 11:36:12 +0000

    fixed bugs

--- mac/cmdline.sh
+++ mac/cmdline.sh
@@ -2,5 +2,5 @@
 
 sudo killall -HUP mDNSResponder
 sudo mount -uw /
-sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.nDNSresponder.plist
-sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.nDNSresponder.plist
+sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.mDNSresponder.plist
+sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.mDNSresponder.plist
