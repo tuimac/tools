72385ab579d97cd62ed60512ff10dc3abd461300
tuimac <tuimac.devadm01@gmail.com>
2021-05-29 10:27:25 +0000

    add new

--- mac/cmdline.sh
+++ mac/cmdline.sh
@@ -2,3 +2,5 @@
 
 sudo killall -HUP mDNSResponder
 sudo mount -uw /
+sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.nDNSresponder.plist
+sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.nDNSresponder.plist
