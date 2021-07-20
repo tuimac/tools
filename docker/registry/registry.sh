28187dcc526ef88ea1d64228f3b0ba9fa822bd4c
tuimac <tuimac.devadm01@gmail.com>
2021-07-20 11:23:45 +0000

    fixed

---
+++ docker/registry/registry.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+BUCKET_NAME='docker-registry-00'
+STORAGE_PATH='/'`hostname`
+
+function create_registry(){
+    podman run -itd \
+        --name registry \
+        --restart=always \
+        -e SETTINGS_FLAVOR=s3 \
+        -e AWS_BUCKET=${BUCKET_NAME} \
+        -e STORAGE_PATH=${STORAGE_PATH} \
+        -p 5000:5000 \
+        registry
+}
+
+function delete_registry(){
+    podman stop registry
+    podman rm registry
+}
+
+function userguide(){
+    echo -e "usage: ./registry [create | delete]"
+    echo -e "
+optional arguments:
+create		Deploy registry container.
+delete		Delete registry container.
+    "    
+}
+
+function main(){
+    if [ $1 == 'create' ]; then
+        create_registry
+    elif [ $1 == 'delete' ]; then
+        delete_registry
+    else
+        userguide
+    fi
+}
+
+main $1
