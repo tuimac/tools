04bb6441586bb70ddcfab5c8fd503a65487ef9b0
tuimac <tuimac.devadm01@gmail.com>
2021-07-23 01:54:30 +0000

    update tls

--- docker/registry/dockerregistry.sh
+++ docker/registry/dockerregistry.sh
@@ -2,22 +2,69 @@
 
 BUCKET_NAME='docker-registry-00'
 STORAGE_PATH='/'`hostname`
+DOMAIN='registry.tuimac.me'
+
+function disableSELinux(){
+    local result=`sudo getenforce`
+    [[ $result == 'Enforcing' ]] && { sudo setenforce 0; }
+}
+
+function genCert(){
+    mkdir -p certs
+    openssl req \
+        -newkey rsa:4096 -nodes -sha256 -keyout certs/domain.key \
+        -addext "subjectAltName = DNS:"${DOMAIN} \
+        -x509 -days 3650 -out certs/domain.crt
+}
 
 function create_registry(){
-
+    cat <<EOF> config.yml
+version: 0.1
+log:
+  accesslog:
+    disabled: false
+  level: info
+  formatter: json
+storage:
+  s3:
+    region: ap-northeast-1
+    bucket: $BUCKET_NAME
+    encrypt: false
+    secure: true
+    v4auth: true
+    chunksize: 5242880
+    multipartcopychunksize: 33554432
+    multipartcopymaxconcurrency: 100
+    multipartcopythresholdsize: 33554432
+    rootdirectory: $STORAGE_PATH
+http:
+  addr: :443
+  headers:
+    X-Content-Type-Options: [nosniff]
+  tls:
+    certificate: /certs/domain.crt
+    key: /certs/domain.key
+health:
+  storagedriver:
+    enabled: true
+    interval: 10s
+    threshold: 3
+EOF
     docker run -itd \
         --name registry \
         --restart=always \
-        -e SETTINGS_FLAVOR=s3 \
-        -e AWS_BUCKET=${BUCKET_NAME} \
-        -e STORAGE_PATH=${STORAGE_PATH} \
-        -p 5000:5000 \
+        -p 443:443 \
+        -v $(pwd)/certs:/certs \
+        -v $(pwd)/config.yml:/etc/docker/registry/config.yml \
         registry
+    [[ $? -eq 0 ]] && { rm config.yml; }
 }
 
 function delete_registry(){
     docker stop registry
     docker rm registry
+    rm -rf certs/
+    rm -f  config.yml
 }
 
 function userguide(){
@@ -30,7 +77,10 @@ delete		Delete registry container.
 }
 
 function main(){
+    [[ -z $1 ]] && { userguide; exit 1; }
+    disableSELinux
     if [ $1 == 'create' ]; then
+        genCert
         create_registry
     elif [ $1 == 'delete' ]; then
         delete_registry
