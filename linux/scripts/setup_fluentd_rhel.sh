982187077b55797b05e4f9314fdee8add08a3a02
tuimac <tuimac.devadm01@gmail.com>
2021-08-18 04:44:39 +0000

    fixed

--- linux/scripts/setup_fluentd_rhel.sh
+++ linux/scripts/setup_fluentd_rhel.sh
@@ -3,3 +3,4 @@
 curl -L https://toolbelt.treasuredata.com/sh/install-redhat-td-agent4.sh | sudo sh
 #sudo systemctl start td-agent.service
 sudo systemctl status td-agent.service
+sudo td-agent-gem install fluent-plugin-kinesis
