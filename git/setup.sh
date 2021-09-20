8b7fcc217890d4be93bf089630c54a245aed337f
tuimac <tuimac.devadm01@gmail.com>
2021-09-20 20:58:36 +0900

    delete

--- git/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-GITUSERNAME='tuimac'
-PASSWORD=`curl http://node3/githubtoken`
-NETRC='~/.netrc'
-
-echo -en "machine github.com\nlogin ${GITUSERNAME}\npassword ${PASSWORD}\nprotocol https" > $NETRC
