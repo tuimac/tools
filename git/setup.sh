56c46f20de1b613f69268dcad6287728aa7dcbe6
tuimac <tuimac.devadm01@gmail.com>
2021-09-20 21:04:31 +0900

    add new

---
+++ git/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+GITUSERNAME='tuimac'
+NETRC='~/.netrc'
+
+echo -en "GitHub Password: "
+read -s PASSWORD
+
+echo -en "machine github.com\nlogin ${GITUSERNAME}\npassword ${PASSWORD}\nprotocol https" > $NETRC
