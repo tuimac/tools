d84077ae885dba466c140eb4feaade310b9c508c
Ubuntu <ubuntu@kubernetes.tuimac.private>
2021-09-25 08:53:13 +0000

    save

--- python/dynamic-dns.py
+++ python/dynamic-dns.py
@@ -1,113 +1,79 @@
 #!/usr/bin/env python3
 
 import boto3
-import json
 import sys
 import os
-from datetime import date, datetime
-import re
 import shutil
 import subprocess
 import traceback
+import hashlib
+
+CONF_PATH = 'tuimac.private'
 
 def check_auth():
     if os.geteuid() != 0:
         args = ['sudo', sys.executable] + sys.argv + [os.environ]
         os.execlpe('sudo', *args)
 
-def find_value(json_data, key):
-    if type(json_data) is dict:
-        for k, v in json_data.items():
-            if k == key: return v
-            result = find_value(v, key)
-            if result is not None: return result
-    elif type(json_data) is list:
-        for item in json_data:
-            result = find_value(item, key)
-            if result is not None: return result
-
 def roll_back(new, old):
     os.rename(old, new)
     return
 
-def create_record_data(ec2info):
-    records_new = dict()
-    for instance in ec2info["Reservations"]:
-        status = instance["Instances"][0]["State"]["Name"]
-        for tag in find_value(instance, "Tags"):
-            if not (status == "running" or status == "stopped"): continue
-            if tag["Key"] == "Name":
-                try:
-                    for eni in instance["Instances"][0]["NetworkInterfaces"]:
-                        deviceIndex = eni["Attachment"]["DeviceIndex"]
-                        if deviceIndex > 0:
-                            records_new.setdefault(eni["PrivateIpAddress"], tag["Value"] + str(deviceIndex))
-                        else:
-                            records_new.setdefault(eni["PrivateIpAddress"], tag["Value"])
-                        try:
-                            records_new.setdefault(eni["Association"]["PublicIp"], tag["Value"] + "-public")
-                        except KeyError:
-                            pass
-                except:
-                    pass
-                    #traceback.print_exc()
-    return records_new
+def create_new_records(ec2):
+    # List ec2 information only from only running instances
+    ec2_list = ec2.describe_instances(
+        Filters = [
+            {
+                'Name': 'instance-state-name',
+                'Values': ['running']
+            }
+        ]
+    )
+    print(ec2_list)
 
-def read_record(path):
-    records_old = dict()
-    dumped_dnsconf = list()
-    index = 0
+def rewrite_records(dns_records):
+    pass
 
-    with open(path, 'r') as file:
-        dumped_dnsconf = file.readlines()
-    for i in range(len(dumped_dnsconf)):
-        if re.match("^;", dumped_dnsconf[i]) is not None:
-            index = i + 1
-    if (index + 1) == len(dumped_dnsconf): return
-    for i in range(index, len(dumped_dnsconf)):
-        element_of_record = dumped_dnsconf[i].split()
-        if len(element_of_record) == 0: break
-        records_old.setdefault(element_of_record[3], element_of_record[0])
-    with open(path, 'w') as file:
-        for i in range(index):
-            file.write(dumped_dnsconf[i])
-    return records_old
-
-def compare_records(new, old):
-    if old is None: return new
+def is_same_file(new_file_path, old_file_path):
+    new_file = open(new_file_path, 'r')
+    new_file_hash = hashlib.md5(new_file.read()).hexdigest()
+    old_file = open(old_file_path, 'r')
+    old_file_hash = hashlib.md5(old_file.read()).hexdigest()
+    print(new_file_hash)
+    print(old_file_hash)
+    if new_file_hash == old_file_hash:
+        return True
     else:
-        records_new = old
-        for ip, name in old.items():
-            if ip not in new: records_new.pop(ip)
-        for ip, name in new.items():
-            records_new.setdefault(ip, name)
-        return records_new
+        return False
 
-def renew_bind_records(path, records_new):
-    for ip, name in records_new.items():
-        record = name + " IN " + "A " + ip + "\n"
-        with open(path, 'a') as file:
-            file.write(record)
-    with open(path, 'a') as file:
-        file.write("\n")
-    return
 
 if __name__ == '__main__':
-    check_auth()
-    os.environ['AWS_DEFAULT_REGION'] = "ap-northeast-3"
-    NAMEDCONF = "/etc/named/tuimac.private"
-    TMPFILE = NAMEDCONF + "_old"
-    shutil.copyfile(NAMEDCONF, TMPFILE)
-
     try:
+        # Check if this script was executed by root or sudo
+        check_auth()
+
+        # Set region for boto3
+        os.environ['AWS_DEFAULT_REGION'] = 'ap-northeast-1'
+
+        # Avoid old file to ~.old
+        shutil.copyfile(CONF_PATH, CONF_PATH + '.old')
+
+        # Initialize aws sdk
         ec2 = boto3.client('ec2')
-        ec2info = ec2.describe_instances()
-        records_new = create_record_data(ec2info)
-        records_old = read_record(NAMEDCONF)
-        records_new = compare_records(records_new, records_old)
-        renew_bind_records(NAMEDCONF, records_new)
-        os.remove(TMPFILE)
-        restart_named = "systemctl restart named"
-        subprocess.call(restart_named.split())
-    except IndexError:
-        roll_back(NAMEDCONF, TMPFILE)
+
+        # Create new DNS record
+        dns_records = create_new_records(ec2)
+
+        # Rewrite record
+        rewrite_records(dns_records)
+
+        # If there is some renewable records, reload named
+        if is_same_file(CONF_PATH, CONF_PATH + '.old') is True:
+            print('True')
+            #subprocess.call(['systemctl', 'restart', 'named'])
+
+        # Delete old file
+        os.remove(CONF_PATH + '.old')
+    except:
+        traceback.print_exc()
+        roll_back(CONF_PATH, CONF_PATH + '.old')
