7296af082b24d33278e77bb80be34c12d3c8e17b
EC2 Default User <ec2-user@ip-10-0-0-36.ap-northeast-3.compute.internal>
2022-02-08 03:20:26 +0000

    done

--- aws/datasync/create_task.sh
+++ aws/datasync/create_task.sh
@@ -8,19 +8,23 @@ LOGS_ARN=''
 LOOP=$((60 / INTERVAL))
 
 function create_task(){
+    set -f
+    local name='test-'${1}
+    local schedule='ScheduleExpression="cron('${1}' * * * ? *)"'
+    echo $schedule
     aws datasync create-task \
         --source-location-arn $SOURCE_ARN \
         --destination-location-arn $DEST_ARN \
         --cloud-watch-log-group-arn $LOGS_ARN \
-        --name 'test-'${$1} \
-        -schedule ${1}' * * * * *' \
-        --options 'PreserveDeletedFiles=REMOVE,LogLevel=BASIC,TransferMode=ALL,OverwriteMode=ALWAYS'
+        --name $name \
+        --schedule "$schedule" \
+        --options 'PreserveDeletedFiles=REMOVE,LogLevel=TRANSFER,VerifyMode=ONLY_FILES_TRANSFERRED,OverwriteMode=ALWAYS'
 }
 
 function main(){
     for (( i=0; i<$LOOP; i++ )); do
-        create_task $i
+	local interval=$((i * INTERVAL))
+        create_task $interval
     done
 }
-#main
-create_task
+main
