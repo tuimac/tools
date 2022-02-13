8b34a371e4cc41c60f5a388f0892c04cfc010d0b
tuimac <tuimac.devadm01@gmail.com>
2022-02-13 11:52:31 +0900

    save

--- aws/cloudformation/launch.sh
+++ aws/cloudformation/launch.sh
@@ -2,4 +2,4 @@
 
 [[ -z $1 ]] && { echo 'Need template file name!!'; exit 1; }
 
-aws cloudformation create-stack --stack-name test --region ap-northeast-1 --capabilities CAPABILITY_NAMED_IAM --template-body file://$1
+aws cloudformation create-stack --stack-name test --region ap-northeast-3 --capabilities CAPABILITY_NAMED_IAM --template-body file://$1
