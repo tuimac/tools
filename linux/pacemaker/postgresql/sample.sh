1d8acb59d21735b7ca07545512246089b8550bac
tuimac <tuimac.devadm01@gmail.com>
2022-02-20 07:48:47 +0000

    add to set password for hacluster

--- linux/pacemaker/postgresql/sample.sh
+++ linux/pacemaker/postgresql/sample.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker inspect --type=container --format {{.State.Status}} postgresql 2>/dev/null
+status=$(docker inspect --type=container --format {{.State.Status}} postgresql 2>/dev/null)
+test "$status" = "runninge"
