e246c2d5fe32e76e0dcc06109b9e00428fa7815e
tuimac <tuimac.devadm01@gmail.com>
2022-02-23 04:09:22 +0000

    add global settings

--- git/setup.sh
+++ git/setup.sh
@@ -7,4 +7,7 @@ echo -en "GitHub Password: "
 read -s PASSWORD
 echo ''
 
+git config --global user.email "tuimac.devadm01@gmail.com"
+git config --global user.name "tuimac"
+
 echo -en "machine github.com\nlogin ${GITUSERNAME}\npassword ${PASSWORD}\nprotocol https" > $NETRC
