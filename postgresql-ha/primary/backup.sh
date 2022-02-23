b5598d3cba7d9d0a656cbfce3aa89a96c452bec5
tuimac <tuimac.devadm01@gmail.com>
2022-02-23 10:28:10 +0000

    save

--- postgresql-ha/primary/backup.sh
+++ postgresql-ha/primary/backup.sh
@@ -2,3 +2,6 @@
 
 COMMAND='pg_basebackup -D /var/lib/pgsql/backup -Fp -Xs -P -R'
 podman exec -it -u postgres postgresql ${COMMAND}
+sudo zip -r backup.zip backup/*
+aws s3 cp backup.zip s3://tuimac000
+rm -f backup.zip
