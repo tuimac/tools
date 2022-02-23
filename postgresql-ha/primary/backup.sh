faa6b8eb608c504fd70f607c356a55b87acccd8b
tuimac <tuimac.devadm01@gmail.com>
2022-02-23 10:21:10 +0000

    save

--- postgresql-ha/primary/backup.sh
+++ postgresql-ha/primary/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-COMMAND='pg_basebackup -D /var/lib/pgsql/backup -F tar -z'
+COMMAND='pg_basebackup -D /var/lib/pgsql/backup -Fp -Xs -P -R'
 podman exec -it -u postgres postgresql ${COMMAND}
