26d6b8347e02e998368d0a26675e08aee80ac854
tuimac <tuimac.devadm01@gmail.com>
2022-02-27 06:11:58 +0000

    backup has been done

--- postgresql-ha/containers/docker/primary/backup.sh
+++ postgresql-ha/containers/docker/primary/backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-COMMAND='pg_basebackup -D /var/lib/postgresql/backup -Fp -Xs -P -R'
+sudo rm -rf backup/*
+COMMAND='pg_basebackup -D /var/lib/postgresql/backup -Fp -Xs -P -R -U test'
 podman exec -it -u postgres postgresql ${COMMAND}
 sudo zip -r backup.zip backup/*
 aws s3 cp backup.zip s3://tuimac000
