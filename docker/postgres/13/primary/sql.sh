fac7bad30ff72b6c0b92a9875d579e3f6f06cbc0
tuimac <tuimac.devadm01@gmail.com>
2022-03-13 07:04:36 +0000

    add logging configuration on primary

--- docker/postgres/13/primary/sql.sh
+++ docker/postgres/13/primary/sql.sh
@@ -2,5 +2,5 @@
 
 export PGPASSWORD='password'
 
-#psql -d test -U test -h localhost -c 'CREATE TABLE IF NOT EXISTS ITEMS (name varchar(255) PRIMARY KEY,count int);'
-psql -d test -U test -h localhost -c 'DROP TABLE ITEMS;'
+psql -d test -U test -h localhost -c 'CREATE TABLE IF NOT EXISTS ITEMS (name varchar(255) PRIMARY KEY,count int);'
+#psql -d test -U test -h localhost -c 'DROP TABLE ITEMS;'
