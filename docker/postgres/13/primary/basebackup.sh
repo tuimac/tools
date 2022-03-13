4f3ab2226f395373bfcbf39661669a7db535520e
tuimac <tuimac.devadm01@gmail.com>
2022-03-13 02:44:00 +0000

    save

--- docker/postgres/13/primary/basebackup.sh
+++ docker/postgres/13/primary/basebackup.sh
@@ -2,7 +2,7 @@
 
 sudo rm -rf backup/*
 COMMAND='pg_basebackup -R -D /var/lib/postgresql/backup -U test'
-docker exec -it -u postgres postgresql ${COMMAND}
+docker exec -it postgresql ${COMMAND}
 sudo zip -r backup.zip backup/*
 MD5=`openssl md5 -binary backup.zip | base64`
 aws s3 cp backup.zip s3://tuimac000 --metadata 'md5="'${MD5}'"'
