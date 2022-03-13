9313a5edbf694fb1292540e5bd1881a24f86e07c
tuimac <tuimac.devadm01@gmail.com>
2022-03-13 08:04:49 +0000

    save

--- docker/postgres/13/primary/sql.sh
+++ docker/postgres/13/primary/sql.sh
@@ -2,5 +2,6 @@
 
 export PGPASSWORD='password'
 
-psql -d test -U test -h localhost -c 'CREATE TABLE IF NOT EXISTS ITEMS (name varchar(255) PRIMARY KEY,count int);'
+#psql -d test -U test -h localhost -c 'CREATE TABLE IF NOT EXISTS ITEMS (name varchar(255) PRIMARY KEY,count int);'
 #psql -d test -U test -h localhost -c 'DROP TABLE ITEMS;'
+docker exec -it postgresql psql -U test -c 'select * from pg_create_physical_replication_slot('node1');'
