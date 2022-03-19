937227f6c3c2316a5e2a95906761ed8b1971bc34
tuimac <tuimac.devadm01@gmail.com>
2022-03-19 08:20:14 +0000

    save

---
+++ docker/cicd/runtime/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function create(){
+    docker-compose up -d
+    sudo chown 999:999 -R postgresql/logs
+    sudo chown 101:101 -R nginx/logs
+    sudo chown 101:101 -R nginx/webapps
+}
+
+function delete(){
+    docker-compose down
+    sudo chown $USER:$USER -R postgresql/logs
+    sudo chown $USER:$USER -R nginx/logs
+    sudo chown $USER:$USER -R nginx/webapps
+}
+
+function main(){
+    [[ -z $1 ]] && { userguide; exit 1; }
+    if [ $1 == "create" ]; then
+        create
+    elif [ $1 == "delete" ]; then
+        delete
+    fi
+}
+
+main $1
