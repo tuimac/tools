0e435ebbf39a87b5546f24dffbf0be46007f285a
tuimac <tuimac.devadm01@gmail.com>
2022-03-27 14:12:57 +0000

    save

--- aws/ssm/automation/test.py
+++ aws/ssm/automation/test.py
@@ -3,11 +3,13 @@
 import boto3
 
 ssm = boto3.client('ssm')
-ssm.start_automation_execution(
-    DocumentName = 'AWS-DeleteImage',
-    Parameters = {
-        'ImageId': [
-            'ami-0d82d5be06abc2cf8'
-        ]
-    }
+result = ssm.describe_automation_executions(
+    Filters = [
+        {
+            'Key': 'ExecutionId',
+            'Values': ['1dfbd695-f79a-4e03-8f4b-04588b52e277']
+        }
+    ]
 )
+
+print(result)
