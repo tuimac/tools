c4a037c0c0d5f910ad63ae72bfe24b0f411c2238
EC2 Default User <ec2-user@docker.tuimac.private>
2022-03-27 12:26:24 +0000

    save

--- aws/ssm/automation/test.py
+++ aws/ssm/automation/test.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python3
 
 import boto3
-'''
-ec2 = boto3.client('ec2')
-images = ec2.describe_images(
-    Owners = [
-        'self'
-    ]
-)['Images']
-print(type(images))
-images.sort(key=lambda sort_key: sort_key.get('CreationDate'), reverse=True)
-print(images)
-'''
 
-retention = 3
-
-nums = [1, 3, 5, 6, 8]
-result = nums[retention:]
-print(result)
+ssm = boto3.client('ssm')
+ssm.start_automation_execution(
+    DocumentName = 'AWS-DeleteImage',
+    Parameters = {
+        'ImageId': [
+            'ami-0d82d5be06abc2cf8'
+        ]
+    }
+)
