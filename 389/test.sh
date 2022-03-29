40722b8d37f9e78205403164d3a4b120eb9e1a61
tuimac <tuimac.devadm01@gmail.com>
2022-03-29 14:34:19 +0000

    save

--- 389/test.sh
+++ 389/test.sh
@@ -6,7 +6,10 @@ dnf module enable 389-ds -y
 dnf install 389-ds-base 389-ds-base-legacy-tools -y
 setup-ds.pl
 
-#expect -c "
-#set timeout 5
-#spawn setup-ds.pl
-#"
+expect -c "
+set timeout 5
+spawn setup-ds.pl
+expect \"Would you like to continue with set up? \[yes\]:\"
+send \"yes\n\"
+expect 
+"
