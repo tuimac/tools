0144275744a41ebdd7d3b7957844fa6d3408c028
tuimac <tuimac.devadm01@gmail.com>
2022-03-30 13:20:51 +0000

    add to authrize the permission

--- linux/fluentd/install_setup.sh
+++ linux/fluentd/install_setup.sh
@@ -3,6 +3,8 @@
 SYSTEMD='/lib/systemd/system/td-agent.service'
 CONF='/etc/td-agent/td-agent.conf'
 
+[[ $USER != 'root' ]] && { echo -n 'Must be root!'; exit 1; }
+
 curl -L https://toolbelt.treasuredata.com/sh/install-redhat-td-agent4.sh | sudo sh
 sudo systemctl status td-agent.service
 sudo td-agent-gem install fluent-plugin-kinesis
