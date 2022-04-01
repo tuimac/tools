8af72f52a1ae9542b16381d784ea91be3ca2646a
tuimac <tuimac.devadm01@gmail.com>
2022-04-01 23:06:16 +0900

    add new

--- aws/ssm/automation/test.py
+++ aws/ssm/automation/test.py
@@ -1,15 +1,4 @@
 #!/usr/bin/env python3
 
-import boto3
-
-ssm = boto3.client('ssm')
-result = ssm.describe_automation_executions(
-    Filters = [
-        {
-            'Key': 'ExecutionId',
-            'Values': ['1dfbd695-f79a-4e03-8f4b-04588b52e277']
-        }
-    ]
-)
-
-print(result)
+test = [2, 3, 5, 6]
+print([i for i in test if i == 2])
