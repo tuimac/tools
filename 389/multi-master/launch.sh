892d2d71cbcfeb359af106064a859841adbc770b
tuimac <tuimac.devadm01@gmail.com>
2022-04-16 22:05:34 +0900

    move 389ds

--- 389/multi-master/launch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-STACK_NAME='rhel389dsmmr'
-
-aws cloudformation create-stack --stack-name $STACK_NAME --region ap-northeast-3 --capabilities CAPABILITY_NAMED_IAM --template-body file://template.yml
