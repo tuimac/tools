b632aac27a82905d8b117cc889a35d31b8b3ea0f
tuimac <tuimac.devadm01@gmail.com>
2022-04-26 06:00:00 +0000

    save\

--- linux/redhat8.4/ebs-exist-mount.sh
+++ linux/redhat8.4/ebs-exist-mount.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 DEVICE='/dev/nvme1n1'
-MOUNT_POINT='/usr/bin'
-TARGET_UUID='18ff16ce-a2c4-4339-be9f-778375d5378a'
+MOUNT_POINT='/usr/sbin'
+TARGET_UUID='8bf2a307-8044-4786-8c50-76b39bc5ccc3'
 
 [[ $USER != 'root' ]] && { echo 'Must be root!'; exit 1; }
-mount $DEVICE /data
-cp -r ${MOUNT_POINT}/* /data
-umount /data
+mkdir tmpdir
+cp -r ${MOUNT_POINT}/* tmpdir/
+mount $DEVICE $MOUNT_POINT
+cp -r tmpdir/* ${MOUNT_POINT}/
 echo 'UUID='${TARGET_UUID}' '${MOUNT_POINT}'                xfs     defaults,nofail 0 0' >> /etc/fstab
+rm -rf tmpdir
