fcaa6259f304d1b358f47007485196d3019bf45b
tuimac <tuimac.devadm01@gmail.com>
2022-05-03 15:58:41 +0900

    add to delete the unecessary packages

--- aws/cloudwatch/cwagent/install-cwagent-arm64.sh
+++ aws/cloudwatch/cwagent/install-cwagent-arm64.sh
@@ -6,6 +6,7 @@ function ubuntu-install(){
     wget https://s3.${REGION}.amazonaws.com/amazoncloudwatch-agent-${REGION}/ubuntu/arm64/latest/amazon-cloudwatch-agent.deb
     dpkg -i -E ./amazon-cloudwatch-agent.deb
     /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m onPremise -s -c ssm:home
+    rm -f amazon-cloudwatch-agent.deb
 }
 
 function main(){
