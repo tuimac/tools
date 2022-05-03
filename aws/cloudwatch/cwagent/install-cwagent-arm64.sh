31c00a66d120f8aa4083e42932600d843d98904a
tuimac <tuimac.devadm01@gmail.com>
2022-05-03 16:06:05 +0900

    fixed

--- aws/cloudwatch/cwagent/install-cwagent-arm64.sh
+++ aws/cloudwatch/cwagent/install-cwagent-arm64.sh
@@ -5,6 +5,7 @@ REGION='ap-northeast-3'
 function ubuntu-install(){
     wget https://s3.${REGION}.amazonaws.com/amazoncloudwatch-agent-${REGION}/ubuntu/arm64/latest/amazon-cloudwatch-agent.deb
     dpkg -i -E ./amazon-cloudwatch-agent.deb
+    aws configure --profile AmazonCloudWatchAgent
     /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m onPremise -s -c ssm:home
     rm -f amazon-cloudwatch-agent.deb
 }
