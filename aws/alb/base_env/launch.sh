b870fcd837751290563aaf6be2dd2172117e6c76
tuimac <tuimac.devadm01@gmail.com>
2022-05-28 16:27:16 +0900

    save

---
+++ aws/alb/base_env/launch.sh
@@ -0,0 +1,4 @@
+STACK_NAME='SorryAlb'
+REGION='ap-northeast-3'
+
+aws cloudformation create-stack --stack-name $STACK_NAME --region ${REGION} --capabilities CAPABILITY_NAMED_IAM --template-body file://template.yml
