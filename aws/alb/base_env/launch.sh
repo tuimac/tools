2b89ad36bdb4d305b34e29248452d50ef7004e34
tuimac <tuimac.devadm01@gmail.com>
2022-05-29 14:05:12 +0900

    upgraded

--- aws/alb/base_env/launch.sh
+++ aws/alb/base_env/launch.sh
@@ -1,4 +1,4 @@
-STACK_NAME='SorryAlb'
+STACK_NAME='alb-test'
 REGION='ap-northeast-3'
 
 aws cloudformation create-stack --stack-name $STACK_NAME --region ${REGION} --capabilities CAPABILITY_NAMED_IAM --template-body file://template.yml
