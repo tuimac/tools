e9d42dcb416fc667e9059f6f4baaadeaa5d20897
tuimac <tuimac.devadm01@gmail.com>
2022-10-06 16:53:51 +0000

    save

--- docker/postgres/14/primary/chech_replication.sh
+++ docker/postgres/14/primary/chech_replication.sh
@@ -2,8 +2,8 @@
 
 export PGPASSWORD='password'
 
-podman exec postgresql psql -U test -h localhost -c 'select * from pg_stat_replication;'
+psql -U test -d test -h localhost -c 'select * from pg_stat_replication;'
 echo '########################################################################################'
-podman exec postgresql psql -t -U test -h localhost -c "select sync_state from pg_stat_replication where application_name = 'node1';"
+psql -t -U test -d test -h localhost -c "select sync_state from pg_stat_replication where application_name = 'node1';"
 echo '########################################################################################'
-podman exec postgresql psql -U test -h localhost -c 'select * from pg_stat_activity;'
+psql -U test -d test -h localhost -c 'select * from pg_stat_activity;'
