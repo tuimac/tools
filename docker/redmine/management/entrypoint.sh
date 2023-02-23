1f3f5ed7c0159e645ad68a966ef159417470281d
tuimac <tuimac.devadm01@gmail.com>
2023-02-23 05:19:05 +0000

    sae

--- docker/redmine/management/entrypoint.sh
+++ docker/redmine/management/entrypoint.sh
@@ -2,6 +2,6 @@
 
 while true; do
     sleep 60
-    mysqldump -u root -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOSTNAME} --databases ${MYSQL_DATABASE} > ${BACKUP_FILE_NAME}
+    mysqldump -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -h ${MYSQL_HOSTNAME} --databases ${MYSQL_DATABASE} > ${BACKUP_FILE_NAME}
     aws s3 cp ${BACKUP_FILE_NAME} ${BACKUP_S3_FILE_PATH}
 done
--- docker/redmine/mysql/scripts/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "GRANT ALL ON *.* TO '"$MYSQL_USER"'@'%' ;" | "${mysql[@]}"
-echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
