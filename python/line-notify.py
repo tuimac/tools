e970494f896076bfbd0a79b45c93674557a167a6
tuimac <tuimac.devadm01@gmail.com>
2023-08-20 05:37:24 +0000

    change to urllib from requests

--- python/line-notify.py
+++ python/line-notify.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
 
-import requests
+import urllib.request
+import urllib.parse
 
 TOKEN = ''
 URL = 'https://notify-api.line.me/api/notify'
 
 def send_notify(message):
-    requests.post(
-        URL,
-        headers = {
-            'Authorization': f'Bearer {TOKEN}'
-        },
-        data = {
-            'message': f'message: {message}'
-        }
-    )
+    headers = { 'Authorization': f'Bearer {TOKEN}', 'Content-Type': 'application/x-www-form-urlencoded' }
+    payload = { 'message': f'message: {message}' }
+
+    request = urllib.request.Request(URL, urllib.parse.urlencode(payload).encode('utf-8'), headers, method='POST')
+    with urllib.request.urlopen(request) as response:
+        pass
 
 if __name__ == '__main__':
     message = 'test'
